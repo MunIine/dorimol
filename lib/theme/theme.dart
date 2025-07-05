45394414ae5f186e047c8c21b7a7a29fd049ad46
Hidanath <steevpixel@gmail.com>
2025-07-05 21:47:24 +0300

    create theme

---
+++ lib/theme/theme.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+const brandColor = Color(0xFF00b956);
+const brand20Color = Color(0xFFCCF1DD);
+const backgroundColor = Color(0xFFFCFCFC);
+const textBlackColor = Color(0xFF2B2B2B);
+const textGreyColor = Color(0xFF303030);
+
+
+final lightTheme = ThemeData(
+  useMaterial3: true,
+  colorScheme: ColorScheme.fromSeed(seedColor: brandColor),
+  scaffoldBackgroundColor: backgroundColor,
+  fontFamily: "Rostelecom"
+);
---
+++ lib/widgets/svg_icons.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class SvgIcons {
+  static const String _fontFamily = 'MyIcons';
+
+  static const IconData bookmark = IconData(0xe900, fontFamily: _fontFamily);
+  static const IconData filter = IconData(0xe901, fontFamily: _fontFamily);
+  static const IconData message = IconData(0xe902, fontFamily: _fontFamily);
+  static const IconData minus = IconData(0xe903, fontFamily: _fontFamily);
+  static const IconData plus = IconData(0xe904, fontFamily: _fontFamily);
+  static const IconData shoppingCart = IconData(0xe905, fontFamily: _fontFamily);
+  static const IconData sliders = IconData(0xe906, fontFamily: _fontFamily);
+  static const IconData trash = IconData(0xe907, fontFamily: _fontFamily);
+}
