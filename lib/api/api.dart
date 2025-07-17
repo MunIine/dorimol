4074650d3206eb89d70a3cbacd9b139837fe7d8d
Hidanath <steevpixel@gmail.com>
2025-07-17 14:41:11 +0300

    Add api client

---
+++ lib/api/api.dart
@@ -0,0 +1,20 @@
+import 'package:dio/dio.dart';
+import 'package:dorimol/api/models/category.dart';
+import 'package:retrofit/retrofit.dart';
+
+part 'api.g.dart';
+
+@RestApi(baseUrl: "")
+abstract class DorimolApiClient {
+  factory DorimolApiClient(Dio dio, {String baseUrl}) = _DorimolApiClient;
+  factory DorimolApiClient.create({String? apiUrl}){
+    final dio = Dio();
+    if (apiUrl != null) {
+      return DorimolApiClient(dio, baseUrl: apiUrl);
+    }
+    return DorimolApiClient(dio);
+  }
+
+  @GET('/categories')
+  Future<List<Category>> getCategories();
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,18 @@
+import 'package:dio/dio.dart';
+import 'package:dorimol/api/api.dart';
 import 'package:dorimol/screens/categories/categories_screen.dart';
 import 'package:dorimol/theme/theme.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: ".env");
+
+  final client = DorimolApiClient.create(apiUrl: dotenv.env['API_URL']);
+
   runApp(const MyApp());
 }
-
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
