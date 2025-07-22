690a408848b1888744fd76d6ee6d8ef254d432a6
Hidanath <steevpixel@gmail.com>
2025-07-22 16:34:25 +0300

    Add privacy policy

--- lib/screens/order_form/widgets/order_button.dart
+++ lib/screens/order_form/widgets/order_button.dart
@@ -1,5 +1,7 @@
 import 'package:dorimol/theme/export.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class OrderButton extends StatelessWidget {
   const OrderButton({
@@ -17,26 +19,46 @@ class OrderButton extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: EdgeInsets.symmetric(horizontal: 16).copyWith(bottom: 20),
-      child: GestureDetector(
-        onTap: onTap,
-        child: Container(
-          height: 50,
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(12),
-            color: colorTheme.seedColor
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          GestureDetector(
+            onTap: onTap,
+            child: Container(
+              height: 50,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(12),
+                color: colorTheme.seedColor
+              ),
+              child: Row(
+                children: [
+                  Spacer(),
+                  Text("$priceруб.", style: AppText.h1.copyWith(color: Color(0xFFFFFFFF))),
+                  SizedBox(width: 16),
+                  Text("|", style: AppText.h1.copyWith(color: Color(0xFFFFFFFF))),
+                  SizedBox(width: 16),
+                  Text("Заказать", style: AppText.h1.copyWith(color: Color(0xFFFFFFFF))),
+                  Spacer()
+                ],
+              )
+            ),
           ),
-          child: Row(
-            children: [
-              Spacer(),
-              Text("$priceруб.", style: AppText.h1.copyWith(color: Color(0xFFFFFFFF))),
-              SizedBox(width: 16),
-              Text("|", style: AppText.h1.copyWith(color: Color(0xFFFFFFFF))),
-              SizedBox(width: 16),
-              Text("Заказать", style: AppText.h1.copyWith(color: Color(0xFFFFFFFF))),
-              Spacer()
-            ],
+          SizedBox(height: 14),
+          RichText(
+            textAlign: TextAlign.center,
+            text: TextSpan(
+              style: AppText.t1.copyWith(fontFamily: AppText.rostelecom.fontFamily),
+              children: [
+                TextSpan(text: "Оформляя заказ, вы автоматически соглашаетесь на обработку\n",style: TextStyle(color: colorTheme.textGray)),
+                TextSpan(
+                  text: "персональных данных", 
+                  style: TextStyle(color: colorTheme.seedColor),
+                  recognizer: TapGestureRecognizer()..onTap = () => launchUrl(Uri.parse("https://ecobaza.github.io/EcoBaza_policy/"))
+                ),
+              ]
+            )
           )
-        ),
+        ],
       ),
     );
   }
